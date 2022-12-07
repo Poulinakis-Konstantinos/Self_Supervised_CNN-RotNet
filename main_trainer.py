58dcda76bf3bad7f57ebc0e084161463aedf271a
Aman Saraf <amansaraf99@gmail.com>
2022-12-06 22:00:40 -0800

    WIP main_trainer, 1 epoch works

--- main_trainer.py
+++ main_trainer.py
@@ -16,6 +16,7 @@ import wandb
 import flax
 from utils.dataloader import load_data
 from utils.flax_utils import rotate_image
+from tqdm import tqdm
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -34,19 +35,19 @@ def parse():
     args = parser.parse_args()
     return args
 
-@jax.jit
-def cross_entropy_loss(*, logits, labels):
+def cross_entropy_loss(*, logits, labels, num_classes):
     """
         Step 3: https://flax.readthedocs.io/en/latest/getting_started.html#define-loss
     """
-    labels_onehot = jax.nn.one_hot(labels, num_classes=10)
+    labels_onehot = jax.nn.one_hot(labels, num_classes=num_classes)
     return optax.softmax_cross_entropy(logits=logits, labels=labels_onehot).mean()
 
-def compute_metrics(*, logits, labels):
+def compute_metrics(logits, labels, num_classes):
     """
         Step 4: https://flax.readthedocs.io/en/latest/getting_started.html#metric-computation
     """
-    loss = cross_entropy_loss(logits=logits, labels=labels)
+    loss = cross_entropy_loss(logits=logits, labels=labels, num_classes=num_classes)
+
     accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
     metrics = {"loss": loss, "accuracy": accuracy}
     return metrics
@@ -61,8 +62,7 @@ def create_train_state(rng, model, learning_rate, momentum):
     # TODO: Check if this is correct for BatchNorm
     return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)
 
-@jax.jit
-def train_batch(state, images, labels):
+def train_batch(state, images, labels, num_classes):
     """
         Step 7: https://flax.readthedocs.io/en/latest/getting_started.html#training-step
     """
@@ -70,52 +70,52 @@ def train_batch(state, images, labels):
         logits, new_state = state.apply_fn(
             {"params": params}, images, mutable=["batch_stats"], train=True
         ) # TODO: Batch Norm??
-        loss = cross_entropy_loss(logits=logits, labels=labels)
+
+        loss = cross_entropy_loss(logits=logits, labels=labels, num_classes=num_classes)
+        
         return loss, (logits, new_state)
     
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
     aux, grads = grad_fn(state.params)
     logits, new_state = aux[1]
-    state = state.apply_gradients(grads=grads, batch_stats=new_state["batch_stats"])
-    metrics = compute_metrics(logits=logits, labels=labels)
+    # TODO: BatchNorm
+    state = state.apply_gradients(grads=grads)
+    metrics = compute_metrics(logits=logits, labels=labels, num_classes=num_classes)
     return state, metrics
 
-def train_epoch(state, dataloader, rot_train=False):
+def train_epoch(state, dataloader, rot_train=False, num_classes=10):
     """
         Step 9: https://flax.readthedocs.io/en/latest/getting_started.html#train-function
     """
     # TODO: Shuffle Please!!
     batch_metrics = []
+    i = 0
     for images, labels in dataloader:
+        print(i, "/", len(dataloader))
+        i += 1
         # --------- Change the labels and modify batch for backbone training --------- #
-        # TODO: Not the most efficient implementation, should be done once at load time.
-        if rot_train:
-            rotated_images, rotated_labels = rotate_image(images)
-            state, metrics = train_batch(state, rotated_images, rotated_labels)
-        else:
-            state, metrics = train_batch(state, images, labels)
+        state, metrics = train_batch(state, images, labels, num_classes=num_classes)
         batch_metrics.append(metrics)
     batch_metrics_np = jax.device_get(batch_metrics)
     epoch_metrics_np = {k: np.mean([metrics[k] for metrics in batch_metrics_np]) for k in batch_metrics_np[0]}
     return state, epoch_metrics_np
 
-@jax.jit
-def eval_batch(state, images, labels):
+def eval_batch(state, images, labels, num_classes):
     """
         Step 8: https://flax.readthedocs.io/en/latest/getting_started.html#evaluation-step
     """
     logits = state.apply_fn(
-        {"params": state.params, "batch_stats": state.batch_stats}, images, mutable=False, train=False
+        {"params": state.params}, images, mutable=False, train=False
     )
-    return compute_metrics(logits=logits, labels=labels)
+    return compute_metrics(logits=logits, labels=labels, num_classes=num_classes)
 
-def eval_model(state, dataloader):
+def eval_model(state, dataloader, num_classes=10):
     """
         Step 10: https://flax.readthedocs.io/en/latest/getting_started.html#eval-function
     """
     batch_metrics = []
     for images, labels in dataloader:
-        metrics = eval_batch(state, images, labels)
+        metrics = eval_batch(state, images, labels, num_classes=num_classes)
         batch_metrics.append(metrics)
     batch_metrics_np = jax.device_get(batch_metrics)
     validation_metrics_np = {k: np.mean([metrics[k] for metrics in batch_metrics_np]) for k in batch_metrics_np[0]}
@@ -140,7 +140,7 @@ def main():
     # Step 5: https://flax.readthedocs.io/en/latest/getting_started.html#loading-data
     # NOTE: Choose batch_size and workers based on system specs
     # NOTE: This dataloader requires pytorch to load the datset for convenience.
-    train_loader, validation_loader, test_loader, rot_train_loader, rot_validation_loader, rot_test_loader = load_data(batch_size=128, workers=4)
+    train_loader, validation_loader, test_loader, rot_train_loader, rot_validation_loader, rot_test_loader = load_data(batch_size=args.batch_size, workers=4)
     print("Data Loaded!")
     
     # --- Create the Train State Abstraction (see documentation in link below) --- #
@@ -149,11 +149,11 @@ def main():
     print("Train State Created")
 
     print("Starting Training Loop!")
-    for epoch in range(args.epochs):
+    for epoch in tqdm(range(args.epochs)):
 
         # ------------------------------- Training Step ------------------------------ #
         # Step 7: https://flax.readthedocs.io/en/latest/getting_started.html#training-step
-        state, train_epoch_metrics_np = train_epoch(state, rot_train_loader, rot_train=True)
+        state, train_epoch_metrics_np = train_epoch(state, rot_train_loader, rot_train=True, num_classes=4)
 
         # Print train metrics every epoch
         print(
@@ -164,14 +164,14 @@ def main():
 
         # ------------------------------ Evaluation Step ----------------------------- #
         #  Step 8: https://flax.readthedocs.io/en/latest/getting_started.html#evaluation-step
-        validation_loss, _ = eval_model(state, rot_validation_loader)
+        validation_loss, _ = eval_model(state, rot_validation_loader, num_classes=4)
 
         # Print validation metrics every epoch
         print(f"validation loss: {validation_loss:.4f}")
         
         if epoch % 10 == 0:
             # Print test metrics every nth epoch
-            _, test_accuracy = eval_model(state, rot_test_loader)
+            _, test_accuracy = eval_model(state, rot_test_loader, num_classes=4)
             print(f"test_accuracy: {test_accuracy:.2f}")
 
 

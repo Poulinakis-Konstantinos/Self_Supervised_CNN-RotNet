fba04ac9de1d7d79b3ae9db6d4c758a3697966ca
Jaewon Lee <jwonlee@lbl.gov>
2022-12-07 01:56:39 -0800

    some changes for prednet testing

--- PredNet_jax.py
+++ PredNet_jax.py
@@ -1,5 +1,5 @@
 import jax
-import tensorflow as tf	
+import tensorflow as tf
 from typing import Any, Callable, Sequence, Optional, List
 from jax import lax, random, numpy as jnp
 import flax
@@ -18,7 +18,7 @@ import optax
 from RotNet import RotNet3
 
 
-SAVE_PATH = 'Saved_models'
+SAVE_PATH = "Saved_models"
 
 # Generate key which is used to generate random numbers
 key = random.PRNGKey(1)
@@ -29,13 +29,18 @@ class Head(nn.Module):
     num_classes: int
     cnn_layers: List[int]
     batch_norm_cls: partial = partial(nn.BatchNorm, momentum=0.9)
-    
+
     @nn.compact
     def __call__(self, inputs, train: bool):
         for layers in self.cnn_layers:
-                x = nn.Conv(features=layers, kernel_size=(3, 3), dtype=self.dtype, kernel_init=self.kernel_init)
-                x = nn.relu(x)
-                x = self.batch_norm_cls(use_running_average=not train)(x)
+            x = nn.Conv(
+                features=layers,
+                kernel_size=(3, 3),
+                dtype=self.dtype,
+                kernel_init=self.kernel_init,
+            )
+            x = nn.relu(x)
+            x = self.batch_norm_cls(use_running_average=not train)(x)
         # if shape (32, 32, 3) : need flatten layer
         # if shape(1, 32, 32, 3): not need flatten ?
         x = inputs.reshape(inputs.shape[0], -1)
@@ -50,26 +55,64 @@ class Head(nn.Module):
 
 class TransferModel(nn.Module):
     backbone: nn.Module
+    backbone_params: nn.Params
     head: nn.Module
     cnn_layers: List[int]
     batch_norm_cls: partial = partial(nn.BatchNorm, momentum=0.9)
-    
+
     @nn.compact
     def __call__(self, input, train: bool):
         x = input
         if self.backbone is not None:
-            x = self.backbone(x)
+            x = self.backbone.apply(self.backbone_params, x)
+
         else:
             for layers in self.cnn_layers:
-                x = nn.Conv(features=layers, kernel_size=(3, 3), dtype=self.dtype, kernel_init=self.kernel_init)
+                x = nn.Conv(
+                    features=layers,
+                    kernel_size=(3, 3),
+                    dtype=self.dtype,
+                    kernel_init=self.kernel_init,
+                )
                 x = nn.relu(x)
                 x = self.batch_norm_cls(use_running_average=not train)(x)
         x = self.head(x)
         return x
 
-def PredNet(cnn_layers, dense_layers, in_shape=(32,32,3), classes=10, transfer=True, base_model=None, lr = 0.1, momentum = 0.01):
-    
-    new_head = Head(dense_layers = dense_layers, num_classes = classes)
-    
-    return TransferModel(backbone = base_model, head = new_head, cnn_layers = cnn_layers)
-    
+
+def PredNet(
+    cnn_layers,
+    dense_layers,
+    in_shape=(32, 32, 3),
+    classes=10,
+    transfer=True,
+    base_model=None,
+    lr=0.1,
+    momentum=0.01,
+):
+
+    new_head = Head(dense_layers=dense_layers, num_classes=classes)
+
+    return TransferModel(backbone=base_model, head=new_head, cnn_layers=cnn_layers)
+
+
+def PredNet1(
+    backbone_params=None,
+    cnn_layers=2,
+    dense_layers=1,
+    in_shape=(32, 32, 3),
+    classes=10,
+    transfer=True,
+    base_model=None,
+    lr=0.1,
+    momentum=0.01,
+):
+
+    new_head = Head(dense_layers=dense_layers, num_classes=classes)
+
+    return TransferModel(
+        backbone=base_model,
+        backbone_params=backbone_params,
+        head=new_head,
+        cnn_layers=cnn_layers,
+    )
--- main_trainer.py
+++ main_trainer.py
@@ -19,16 +19,46 @@ from utils.flax_utils import rotate_image
 from tqdm import tqdm
 from flax.training import train_state, checkpoints
 import os
+from PredNet_jax import Head, TransferModel, PredNet1
+from jax_resnet import slice_variables, Sequential
+
 
 class TrainState(train_state.TrainState):
     batch_stats: Any
 
+
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-j", "--workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 4)")
-    parser.add_argument("--epochs", default=100, type=int, metavar="N", help="number of total epochs to run")
-    parser.add_argument("--start_epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)")
-    parser.add_argument("-b", "--batch_size", default=128, type=int, metavar="N", help="mini-batch size per process (default: 128)")
+    parser.add_argument(
+        "-j",
+        "--workers",
+        default=4,
+        type=int,
+        metavar="N",
+        help="number of data loading workers (default: 4)",
+    )
+    parser.add_argument(
+        "--epochs",
+        default=100,
+        type=int,
+        metavar="N",
+        help="number of total epochs to run",
+    )
+    parser.add_argument(
+        "--start_epoch",
+        default=0,
+        type=int,
+        metavar="N",
+        help="manual epoch number (useful on restarts)",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        default=128,
+        type=int,
+        metavar="N",
+        help="mini-batch size per process (default: 128)",
+    )
     parser.add_argument("--model_rotnet", type=str, default="RotNet3")
     parser.add_argument("--model_prednet", type=str, default="PredNet3")
     parser.add_argument("--CIFAR10", type=bool, default=True)
@@ -36,63 +66,82 @@ def parse():
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument("--dtype", type=str, default="fp32")
-    parser.add_argument("--transfer", action='store_true', default=False)
+    parser.add_argument("--transfer", action="store_true", default=False)
     args = parser.parse_args()
     return args
 
+
 def cross_entropy_loss_(logits, labels, num_classes=10):
     """
-        Step 3: https://flax.readthedocs.io/en/latest/getting_started.html#define-loss
+    Step 3: https://flax.readthedocs.io/en/latest/getting_started.html#define-loss
     """
     labels_onehot = jax.nn.one_hot(labels, num_classes=num_classes)
     return optax.softmax_cross_entropy(logits=logits, labels=labels_onehot).mean()
+
+
 cross_entropy_loss = jax.jit(cross_entropy_loss_, static_argnums=2)
+
 
 def compute_metrics_(logits, labels, num_classes):
     """
-        Step 4: https://flax.readthedocs.io/en/latest/getting_started.html#metric-computation
+    Step 4: https://flax.readthedocs.io/en/latest/getting_started.html#metric-computation
     """
     loss = cross_entropy_loss(logits=logits, labels=labels, num_classes=num_classes)
 
     accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
     metrics = {"loss": loss, "accuracy": accuracy}
     return metrics
+
+
 compute_metrics = jax.jit(compute_metrics_, static_argnums=2)
+
 
 def create_train_state(rng, model, learning_rate, momentum):
     """
-        Step 6: https://flax.readthedocs.io/en/latest/getting_started.html#create-train-state
+    Step 6: https://flax.readthedocs.io/en/latest/getting_started.html#create-train-state
     """
-    variables = model.init(rng, jnp.ones((1, 32, 32, 3), dtype=model.dtype), train=False)
-    params, batch_stats = variables['params'], variables['batch_stats']
+    variables = model.init(
+        rng, jnp.ones((1, 32, 32, 3), dtype=model.dtype), train=False
+    )
+    params, batch_stats = variables["params"], variables["batch_stats"]
     tx = optax.sgd(learning_rate, momentum)
     # TODO: Check if this is correct for BatchNorm
-    return TrainState.create(apply_fn=model.apply, params=params, tx=tx, batch_stats=batch_stats)
+    return TrainState.create(
+        apply_fn=model.apply, params=params, tx=tx, batch_stats=batch_stats
+    )
+
 
 def train_batch_(state, images, labels, num_classes=10):
     """
-        Step 7: https://flax.readthedocs.io/en/latest/getting_started.html#training-step
+    Step 7: https://flax.readthedocs.io/en/latest/getting_started.html#training-step
     """
+
     def loss_fn(params):
         logits, new_state = state.apply_fn(
-            {"params": params, "batch_stats": state.batch_stats}, images, mutable=["batch_stats"], train=True
+            {"params": params, "batch_stats": state.batch_stats},
+            images,
+            mutable=["batch_stats"],
+            train=True,
         )
 
         loss = cross_entropy_loss(logits=logits, labels=labels, num_classes=num_classes)
-        
+
         return loss, (logits, new_state)
-    
+
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
     (_, (logits, new_state)), grads = grad_fn(state.params)
     state = state.apply_gradients(grads=grads)
-    state = state.replace(batch_stats=new_state['batch_stats'])
+    state = state.replace(batch_stats=new_state["batch_stats"])
     metrics = compute_metrics(logits=logits, labels=labels, num_classes=num_classes)
     return state, metrics
+
+
 train_batch = jax.jit(train_batch_, static_argnums=3)
+
 
 def train_epoch(state, dataloader, num_classes=10):
     """
-        Step 9: https://flax.readthedocs.io/en/latest/getting_started.html#train-function
+    Step 9: https://flax.readthedocs.io/en/latest/getting_started.html#train-function
     """
     # TODO: Shuffle Please!!
     batch_metrics = []
@@ -101,30 +150,44 @@ def train_epoch(state, dataloader, num_classes=10):
         state, metrics = train_batch(state, images, labels, num_classes=num_classes)
         batch_metrics.append(metrics)
     batch_metrics_np = jax.device_get(batch_metrics)
-    epoch_metrics_np = {k: np.mean([metrics[k] for metrics in batch_metrics_np]) for k in batch_metrics_np[0]}
+    epoch_metrics_np = {
+        k: np.mean([metrics[k] for metrics in batch_metrics_np])
+        for k in batch_metrics_np[0]
+    }
     return state, epoch_metrics_np
+
 
 def eval_batch_(state, images, labels, num_classes=10):
     """
-        Step 8: https://flax.readthedocs.io/en/latest/getting_started.html#evaluation-step
+    Step 8: https://flax.readthedocs.io/en/latest/getting_started.html#evaluation-step
     """
     logits = state.apply_fn(
-        {"params": state.params, "batch_stats": state.batch_stats}, images, mutable=False, train=False
+        {"params": state.params, "batch_stats": state.batch_stats},
+        images,
+        mutable=False,
+        train=False,
     )
     return compute_metrics(logits=logits, labels=labels, num_classes=num_classes)
+
+
 eval_batch = jax.jit(eval_batch_, static_argnums=3)
+
 
 def eval_model(state, dataloader, num_classes=10):
     """
-        Step 10: https://flax.readthedocs.io/en/latest/getting_started.html#eval-function
+    Step 10: https://flax.readthedocs.io/en/latest/getting_started.html#eval-function
     """
     batch_metrics = []
     for images, labels in dataloader:
         metrics = eval_batch(state, images, labels, num_classes=num_classes)
         batch_metrics.append(metrics)
     batch_metrics_np = jax.device_get(batch_metrics)
-    validation_metrics_np = {k: np.mean([metrics[k] for metrics in batch_metrics_np]) for k in batch_metrics_np[0]}
+    validation_metrics_np = {
+        k: np.mean([metrics[k] for metrics in batch_metrics_np])
+        for k in batch_metrics_np[0]
+    }
     return validation_metrics_np["loss"], validation_metrics_np["accuracy"]
+
 
 def main():
     args = parse()
@@ -145,13 +208,20 @@ def main():
     # Step 5: https://flax.readthedocs.io/en/latest/getting_started.html#loading-data
     # NOTE: Choose batch_size and workers based on system specs
     # NOTE: This dataloader requires pytorch to load the datset for convenience.
-    train_loader, validation_loader, test_loader, rot_train_loader, rot_validation_loader, rot_test_loader = load_data(batch_size=args.batch_size, workers=args.workers)
+    (
+        train_loader,
+        validation_loader,
+        test_loader,
+        rot_train_loader,
+        rot_validation_loader,
+        rot_test_loader,
+    ) = load_data(batch_size=args.batch_size, workers=args.workers)
     print("Data Loaded!")
     # --- Create the Train State Abstraction (see documentation in link below) --- #
     # Step 6: https://flax.readthedocs.io/en/latest/getting_started.html#create-train-state
     state = create_train_state(rng, model, args.lr, args.momentum)
     print("Train State Created")
-    
+
     # createing state directory
     ckpt_path = "./ckpts"
     if not os.path.exists(ckpt_path):
@@ -159,17 +229,21 @@ def main():
         print("creating root directory for state")
     else:
         print("find existing root directory for state")
-    
+
     # restore ckpt if specified
     if args.start_epoch > 0:
-        state = checkpoints.restore_checkpoint(ckpt_path, target=state, step=args.start_epoch)
-    
+        state = checkpoints.restore_checkpoint(
+            ckpt_path, target=state, step=args.start_epoch
+        )
+
     print("Starting Training Loop!")
     for epoch in tqdm(range(args.start_epoch + 1, args.epochs + 1)):
 
         # ------------------------------- Training Step ------------------------------ #
         # Step 7: https://flax.readthedocs.io/en/latest/getting_started.html#training-step
-        state, train_epoch_metrics_np = train_epoch(state, rot_train_loader, num_classes=4)
+        state, train_epoch_metrics_np = train_epoch(
+            state, rot_train_loader, num_classes=4
+        )
 
         # Print train metrics every epoch
         print(
@@ -184,15 +258,32 @@ def main():
 
         # Print validation metrics every epoch
         print(f"validation loss: {validation_loss:.4f}")
-        
+
         if epoch % 10 == 0:
             # Print test metrics every nth epoch
             _, test_accuracy = eval_model(state, rot_test_loader, num_classes=4)
             print(f"test_accuracy: {test_accuracy:.2f}")
-            
+
             # ---------------------------- Saving Checkpoints ---------------------------- #
             # ---- https://flax.readthedocs.io/en/latest/guides/use_checkpointing.html --- #
-            checkpoints.save_checkpoint(ckpt_dir=ckpt_path, target=state, step=epoch, overwrite=True, keep=10)
+            checkpoints.save_checkpoint(
+                ckpt_dir=ckpt_path, target=state, step=epoch, overwrite=True, keep=10
+            )
 
-if __name__ == '__main__':
+    # restore the checkpoint for rotnet and it will be used for prednet constructor
+    state = checkpoints.restore_checkpoint(
+        ckpt_path, target=state, step=args.start_epoch
+    )
+    start, end = 0, len(model.layers) - 2
+    prednet_model = PredNet1(
+        base_model=Sequential(model.layers[start:end]),
+        backbone_params=slice_variables(state.params, start, end),
+    )
+    train_loader, validation_loader, test_loader
+
+    state = create_train_state(rng, model, args.lr, args.momentum)
+    print("Train State Created")
+
+
+if __name__ == "__main__":
     main()
